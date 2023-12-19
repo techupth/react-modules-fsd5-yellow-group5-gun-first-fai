5410abfe527d14d5fbc905e5ba0f98b77c0d662f
Gun <cobaltgun2324@gmail.com>
2023-12-19 17:29:30 +0700

    feat: complete assignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider resetCSS={false}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
